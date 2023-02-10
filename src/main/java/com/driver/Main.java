2c2ffb9f6f5fe673ed1f0171cde011236e57ec46
saurabhraulkar <85070638+saurabhraulkar@users.noreply.github.com>
2023-02-10 20:49:52 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("vrushabh");
+        System.out.println(obj.getName());
+
+    }
 }
